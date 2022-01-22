9a815d54cab30ffa75e7d09acd142249a2cf12b5
sudo <s_s_wataru@yahoo.co.jp>
2022-01-22 17:26:46 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,7 +10,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    follow_redirect!
-    assert_template 'users/new'
   end
+
+    test "valid signup information" do
+      get signup_path
+      assert_difference 'User.count', 1 do
+        post users_path, params: { user: { name:  "Example User",
+                                           email: "user@example.com",
+                                           password:              "password",
+                                           password_confirmation: "password" } }
+      end
+      follow_redirect!
+      assert_template 'users/show'
+    end
 end
