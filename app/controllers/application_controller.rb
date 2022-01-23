15f38dd3b57f7e4a1d38053d725d49ad3ebe1cfd
sudowa <s_s_wataru@yahoo.co.jp>
2022-01-23 09:59:56 +0900

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "hello, world!test"
-  end
+  include SessionsHelper
 end
